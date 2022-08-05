7de6a718fcd772ff72e07f57400a26d37e3d05d7
mdestafadilah <desta.08b@gmail.com>
2022-08-06 04:19:32 +0700

    ADDED MOCKUP FRONTEND

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => (
+  <div className="App">
+          <div>
+        <p>React Vitejs Antd template0</p>
+        <p>
+          <Button type="primary">Primary</Button>
+        </p>
+        <br/>
+        <div style={{ padding: 5 }}>
+          User Info
+        </div>
+      </div>
+  </div>
+);
+
+export default App;
