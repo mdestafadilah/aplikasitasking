12e7aae9986047b4a59cfa07b1ece61d6b8ebc6e
mdestafadilah <desta.08b@gmail.com>
2022-08-08 13:43:22 +0700

    MASIH ADA ERROR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Button } from 'antd';
+import TaskForm from './components/TaskForm';
+import "antd/dist/antd.css";
 
 const App = () => (
   <div className="App">
-          <div>
-        <p>React Vitejs Antd template0</p>
-        <p>
-          <Button type="primary">Primary</Button>
-        </p>
-        <br/>
-        <div style={{ padding: 5 }}>
-          User Info
-        </div>
-      </div>
+    <TaskForm />
   </div>
 );
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				// this is required for the SCSS modules
+				find: /^~(.*)$/,
+				replacement: "$1",
+			},
+		],
+    extensions: ['.js', '.jsx', '.less']
+	},
+  base: './',
+});
