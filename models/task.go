2f84b9d866ee0e11601882c452dd64a2c746293b
mdestafadilah <desta.08b@gmail.com>
2022-08-10 02:37:13 +0700

    TASK MODEL

---
+++ models/task.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mdestafadilah/aplikasitasking/database"
+)
+
+// Buat Field
+type Task struct {
+	ID          uint   `gorm:"primaryKey" json:"id"`
+	NamaPegawai string `json:"namapegawai"`
+	DeadLine    string `json:"deadline"`
+	IsiTask     string `json:"isitask"`
+	Completed   bool   `json:"completed"`
+}
+
+// Buat Koneksi
+func GetTask(c *fiber.Ctx) error {
+	db := database.DBConn
+	var tasks []Task
+	db.Find(&tasks)
+	return c.JSON(&tasks)
+}
