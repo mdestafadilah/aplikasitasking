f7be893ec0e7e37ee8729be40a5f799b8513a7c2
mdestafadilah <desta.08b@gmail.com>
2022-08-10 01:29:58 +0700

    LAYOUT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import TaskForm from './components/TaskForm';
+import React from "react";
+import { Row, Col, Layout } from "antd";
 import "antd/dist/antd.css";
+import TaskForm from "./components/TaskForm";
+
+const { Content } = Layout;
 
 const App = () => (
-  <div className="App">
-    <TaskForm />
-  </div>
+	<Layout className="layout">
+		<Content style={{ padding: "0 50px" }}>
+			<Row>
+				<Col>
+					<h1>Aplikasi Tasking Golang &amp; ReactJS</h1>
+				</Col>
+			</Row>
+			<TaskForm />
+		</Content>
+	</Layout>
 );
 
 export default App;
