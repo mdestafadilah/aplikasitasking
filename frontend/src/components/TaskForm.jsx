73298c2199c9596896d92d37d52264def0a3c1a2
mdestafadilah <desta.08b@gmail.com>
2022-08-10 01:42:25 +0700

    ratain

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,60 +1,59 @@
-import React from 'react'
-import { Form, Col, Button, Input, DatePicker } from 'antd'
-import { PlusCircleFilled } from '@ant-design/icons'
+import React from "react";
+import { Form, Col, Button, Input, DatePicker } from "antd";
+import { PlusCircleFilled } from "@ant-design/icons";
 
 const { TextArea } = Input;
 
-const TaskForm = ({onFormSubmit}) => {
-  const [form] = Form.useForm();
-  const onFinish = () => {
-    onFormSubmit({
-      namapegawai: form.getFieldValue('namapegawai'),
-      deadline: form.getFieldValue('deadline'),
-      isitask: form.getFieldValue('isitask'),
-      completed: false
-    })
-    console.log(form.getFieldValue('title'))
-    form.resetFields();
-  }
-  return(
-    <Form
-      form={form}
-      onFinish={onFinish}
-      layout="horizontal"
-      className='todo-form'
-    >
+const TaskForm = ({ onFormSubmit }) => {
+	const [form] = Form.useForm();
+	const onFinish = () => {
+		onFormSubmit({
+			namapegawai: form.getFieldValue("namapegawai"),
+			deadline: form.getFieldValue("deadline"),
+			isitask: form.getFieldValue("isitask"),
+			completed: false,
+		});
+		console.log(form.getFieldValue("title"));
+		form.resetFields();
+	};
+	return (
+		<Form
+			form={form}
+			onFinish={onFinish}
+			layout="horizontal"
+			className="todo-form"
+		>
+			<Form.Item
+				name={"namapegawai"}
+				rules={[{ required: true, message: "Nama Pegawai Wajib Isi" }]}
+			>
+				<Input placeholder="Nama Pegawai" />
+			</Form.Item>
 
-            <Form.Item
-              name={"namapegawai"}
-              rules={[{ required: true, message: 'Nama Pegawai Wajib Isi' }]}
-            >
-              <Input placeholder="Nama Pegawai"/>
-            </Form.Item>
+			<Form.Item
+				name={"deadline"}
+				rules={[{ required: true, message: "Tanggal Deadline" }]}
+			>
+				<DatePicker placeholder="Tanggal Deadline Wajib Isi" />
+			</Form.Item>
 
-            <Form.Item
-              name={"deadline"}
-              rules={[{ required: true, message: 'Tanggal Deadline' }]}
-            >
-              <DatePicker placeholder='Tanggal Deadline Wajib Isi'/>
-            </Form.Item>
+			<Form.Item
+				name={"isitask"}
+				rules={[{ required: true, message: "Isi Task Wajib Isi" }]}
+			>
+				<TextArea placeholder="Keterangan Task!" showCount maxLength={100} />
+			</Form.Item>
 
-            <Form.Item
-                name={"isitask"}
-                rules={[{ required: true, message: 'Isi Task Wajib Isi'}]}
-            >
-              <TextArea placeholder="Keterangan Task!" showCount maxLength={100} />
-            </Form.Item>
+			<Form.Item>
+				<Col xs={24} sm={24} md={7} lg={5} xl={4}>
+					<Button type="primary" htmlType="submit" block>
+						<PlusCircleFilled />
+						Tambah Task
+					</Button>
+				</Col>
+			</Form.Item>
+		</Form>
+	);
+};
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Col xs={24} sm={24} md={7} lg={5} xl={4}>
-            <Button type="primary" htmlType="submit" block>
-              <PlusCircleFilled />Tambah Task
-            </Button>
-          </Col>
-      </Form.Item>
-
-    </Form>
-  )
-}
-
-export default TaskForm
+export default TaskForm;
