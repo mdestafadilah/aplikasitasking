49be024113d54b51fa45899255728b2a8b92c88d
mdestafadilah <desta.08b@gmail.com>
2022-08-10 04:22:15 +0700

    UPDATE - ENVIRONMENT - API ENDPOINT

--- frontend/src/api/TaskApi.jsx
+++ frontend/src/api/TaskApi.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+const baseUrl = `${import.meta.env.VITE_API_URL}/tasks`;
 
-export default function TaskApi() {
-  return (
-    <div>TaskApi</div>
-  )
-}
+export const loadTasks = () => {
+  return fetch(baseUrl).then((res) => res.json());
+};
+
+export const getTask = (id) => {
+  return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+};
+
+export const createTask = (task) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      namapegawai: todo.namapegawai,
+      deadline: todo.deadline,
+      isitask: todo.isitask,
+      completed: todo.completed,
+    }),
+  }).then((res) => res.json());
+};
+
+export const updateTask = (task) => {
+  return fetch(`${baseUrl}/${task.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      namapegawai: todo.namapegawai,
+      deadline: todo.deadline,
+      isitask: todo.isitask,
+      completed: task.completed,
+    }),
+  });
+};
+
+export const deleteTodo = (id) => {
+  return fetch(`${baseUrl}/${id}`, {
+    method: "DELETE",
+  }).then(res => res.json());
+};
