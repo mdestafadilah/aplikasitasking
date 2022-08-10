abe0856fef64e1c54187382ffb21d327f9545a0d
mdestafadilah <desta.08b@gmail.com>
2022-08-11 04:18:37 +0700

    TASK ITEM

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import { Tooltip, Tag, List, Button, Popconfirm, Switch } from 'antd'
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons'
 
-export default function TaskItem() {
+const TaskItem = ({ task, onTaskRemoval, onTaskToggle }) => {
   return (
-    <div>TaskItem</div>
+    <List.Item
+      actions={[
+        <Tooltip
+          title={task.completed ? 'Belum Selesai' : 'Sudah Selesai'}
+        >
+          <Switch 
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            onChange={() => onTaskToggle(task)}
+            defaultChecked={task.completed}
+          />
+        </Tooltip>,
+        <Popconfirm
+          title="Yakin mau dihapus?"
+          onConfirm={() => {
+            onTaskRemoval(task);
+          }}
+        >
+          <Button className='remove-task-button' type="primary" danger>
+            x
+          </Button>
+        </Popconfirm>
+      ]}
+      className="list-item"
+      key={task.id}
+    >
+      <div className='task-item'>
+        <Tag color={task.completed ? 'cyan' : 'red'} className="task-tag">
+          {task.title}
+        </Tag>
+      </div> 
+    </List.Item>
   )
 }
+
+export default TaskItem
